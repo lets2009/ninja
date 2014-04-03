34c15c4b90fb3a31c916ac14252ffecb081935ae
onawa@mileage-techno.com <onawa@mileage0>
2014-04-03 17:50:34 +0900

    date

---
+++ gov_date.rb
@@ -0,0 +1,16 @@
+# coding: sjis
+require 'date'
+$:.unshift File.dirname(__FILE__)
+require 'jyear'
+
+class GovDate
+  def first
+    1
+  end
+  def to_jdate(date)
+    date.to_time.strftime("%K”N%mŒ%d“ú")
+  end
+  def to_date(jdate)
+    Date.parse(jdate).to_s
+  end
+end
